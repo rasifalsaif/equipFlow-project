c2b354ce99b8ee8ef059eff6892c86cfd35632c7
rasifalsaif <rasifalsaif@gmail.com>
2026-05-05 18:04:54 +0600

    feat(frontend): initialize frontend project structure and basic layout

---
+++ frontend/next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.R2_HOSTNAME!,
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [{
+      source: '/api/auth/:path*',
+      destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/auth/:path*`,
+    },]
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
